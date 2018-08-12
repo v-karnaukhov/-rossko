7fef09f99789076a044d6628482e488b0cb4eed2
v-karnauhov <v13karnauhov@gmail.com>
2018-08-12 17:18:26 +0300

    Initial commit.

---
+++ src/PermutationsService/PermutationsService/Controllers/PermutationsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PermutationsService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class PermutationsController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new[] {"value1", "value2"});
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
